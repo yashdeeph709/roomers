175d495c549b8ad23b2466d6f3441a87bb1c4892
nikam_h <nikam_h@HARSHAL-N.India.XoriantCorp.com>
2015-12-07 15:42:47 +0530

    Update POM again..!!

--- src/main/java/com/roommanagement/controllers/CreateRoomController.java
+++ src/main/java/com/roommanagement/controllers/CreateRoomController.java
@@ -2,11 +2,13 @@ package com.roommanagement.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class CreateRoomController {
 
 	@RequestMapping("/hello")
-	public String sayHello(@RequestParam(value="name", defaultValue="Ivan") String name) {
+	public String sayHello(@RequestParam(value="name", defaultValue="hivan") String name) {
 		return "Hello " + name;
 	}
 }
