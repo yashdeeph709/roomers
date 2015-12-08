f6cec5780a0bc58a8f7d7a321f9b1a7f58e6a28f
Yashdeep Hinge <yashdeeph709@gmail.com>
2015-12-08 13:04:53 +0530

    room management testdata and dump added!

--- src/main/java/com/roommanagement/controllers/RoomController.java
+++ src/main/java/com/roommanagement/controllers/RoomController.java
@@ -2,6 +2,7 @@ package com.roommanagement.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,7 @@ import com.roommanagement.collections.RoomCollection;
 import com.roommanagement.services.RoomService;
 
 
+@CrossOrigin
 @RestController
 public class RoomController {
 
--- src/main/java/com/roommanagement/controllers/UsersController.java
+++ src/main/java/com/roommanagement/controllers/UsersController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import com.roommanagement.collections.UserCollection;
 import com.roommanagement.repository.UsersRepository;
 import com.roommanagement.services.UserService;
 
+@CrossOrigin
 @RestController
 public class UsersController {
 
