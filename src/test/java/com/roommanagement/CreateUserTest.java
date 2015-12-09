87f58fb26a8424ded185c81335a51bd0441994b4
urkude_s <urkude_s@SHRUTI-U.India.XoriantCorp.com>
2015-12-09 15:46:01 +0530

    Added setup() and more test cases.

--- src/test/java/com/roommanagement/CreateUserTest.java
+++ src/test/java/com/roommanagement/CreateUserTest.java
@@ -6,9 +6,12 @@ import java.net.URISyntaxException;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import com.roommanagement.beans.Status;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
+
 
 
 public class CreateUserTest {
@@ -29,20 +32,20 @@ public class CreateUserTest {
 		String expected = "{\"name\":\"Palakh\",\"email\":\"palakh@gmail.com\",\"password\":\"Palakh\",\"rights\":\"1\"}";
 
 		ClientResponse response = webResource.type("application/json").post(ClientResponse.class, expected);
-		String actual=response.getEntity(String.class);
+		 String actual=response.getEntity(String.class);
 		System.out.println(actual);
-		assertEquals(actual, "{\"id\":null,\"name\":\"Palakh\",\"email\":palakh@gmail.com}");
+		assertEquals(actual, "{\"status\":\"true\",\"message\":\"Created User name =Palakh\"}");
 	}
 	
 	@Test
 	public void testCreateUserWithNullValues() throws URISyntaxException {
 		
-		String expected = "{\"name\":\"\",\"email\":\"\",\"password\":\"\",\"rights\":\"\"}";
+		String expected = "{\"name\":null,\"email\":null,\"password\":null,\"rights\":null}";
 
 		ClientResponse response = webResource.type("application/json").post(ClientResponse.class, expected);
-		String actual=response.getEntity(String.class);
+		 String actual=response.getEntity(String.class);
 		System.out.println(actual);
-		assertEquals(actual, "{\"id\":null,\"name\":\"\",\"email\":}");
+		assertEquals(actual, "{\"status\":\"true\",\"message\":\"Created User name =null\"}");
 	}
 
 }
