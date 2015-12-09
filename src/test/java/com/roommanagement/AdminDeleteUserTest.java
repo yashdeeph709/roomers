838de16b39da66177598fe12e304d2a26b898354
kumar_ni <kumar_ni@Nihit-K.India.XoriantCorp.com>
2015-12-09 20:28:43 +0530

    added multipal test cases

--- src/test/java/com/roommanagement/AdminDeleteUserTest.java
+++ src/test/java/com/roommanagement/AdminDeleteUserTest.java
@@ -3,6 +3,7 @@ package com.roommanagement;
 import static org.junit.Assert.assertEquals;
 
 import org.json.JSONObject;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.roommanagement.beans.UserBean;
@@ -16,15 +17,35 @@ public class AdminDeleteUserTest {
 	UserBean userBean;
 	UserService userService;
 	JSONObject obj = new JSONObject();
+	Client client ;
+	WebResource webResource;
+	String baseURI;
+	
+	@Before
+	public void setup()
+	{
+		client = Client.create();
+		 baseURI = "http://localhost:8080/RoomManagement/delete";
+		
+	}
+	
 	
 	@Test
-	public void setup(){
-		Client client=Client.create();
-		WebResource webResource=client.resource("http://localhost:8080/RoomManagement/delete/5668049163c75a0e77cb9fdf");
+	public void deleteUserTest(){
+		webResource = client.resource(baseURI+"/566804b063c75a0e77cb9fe0");
 		ClientResponse response = webResource.type("application/json").get(ClientResponse.class);
 		String output=response.getEntity(String.class);
 		System.out.println(output);
 		assertEquals(output,"{\"status\":\"success\",\"message\":\"User Deleted Successfully!\"}");
 			
 		}	
+	
+	@Test
+	public void deleteUserWithoutIdTest(){
+		webResource = client.resource(baseURI);
+		ClientResponse response = webResource.type("application/json").get(ClientResponse.class);
+		String output=response.getEntity(String.class);
+		assertEquals(output,"{\"status\":\"failed\",\"message\":\"User Deleted Successfully!\"}");
+			
+		}
     }
