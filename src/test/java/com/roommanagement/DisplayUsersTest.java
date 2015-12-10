19c804b75002c5d5fbe1530a96e20a623e454cef
Yashdeep Hinge <yashdeeph709@gmail.com>
2015-12-10 10:26:24 +0530

    RestTest added!

--- src/test/java/com/roommanagement/DisplayUsersTest.java
+++ src/test/java/com/roommanagement/DisplayUsersTest.java
@@ -14,7 +14,7 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
-public class RestTest {
+public class DisplayUsersTest {
 
 	Client client;
 	
