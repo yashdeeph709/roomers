5e485ccd95f867f89afe31c702636d1cb739b62b
Yashdeep Hinge <yashdeeph709@gmail.com>
2015-12-12 19:40:57 +0530

    Status for get specific room is changed to accepted!

--- src/main/java/com/roommanagement/controllers/RoomController.java
+++ src/main/java/com/roommanagement/controllers/RoomController.java
@@ -103,7 +103,7 @@ public class RoomController{
 			httpStatus = HttpStatus.NOT_FOUND; 				//If no rooms are there in the db 
 		}
 		else{
-			httpStatus = HttpStatus.FOUND;					//If room are there
+			httpStatus = HttpStatus.ACCEPTED;					//If room are there
 		}
 		
 		HttpHeaders httpHeaders = new HttpHeaders();
