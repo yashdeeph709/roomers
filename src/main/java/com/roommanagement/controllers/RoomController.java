58bafae75b43870fbe894021cf38542163258df4
Yashdeep Hinge <yashdeeph709@gmail.com>
2015-12-12 19:16:02 +0530

    Status for get on rooms is changed to OK!

--- src/main/java/com/roommanagement/controllers/RoomController.java
+++ src/main/java/com/roommanagement/controllers/RoomController.java
@@ -78,7 +78,7 @@ public class RoomController{
 			httpStatus = HttpStatus.NO_CONTENT; 				//If no rooms are there in the db 
 		}
 		else{
-			httpStatus = HttpStatus.FOUND;					//If room are there
+			httpStatus = HttpStatus.OK;					//If room are there
 		}
 		
 		HttpHeaders httpHeaders = new HttpHeaders();
