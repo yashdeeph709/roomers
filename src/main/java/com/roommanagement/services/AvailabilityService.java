4ecd0611b96895477d3fe3e40a2ba6aad00c8854
nikam_h <nikam_h@HARSHAL-N.India.XoriantCorp.com>
2015-12-14 14:14:03 +0530

    Basic Availability Service Implementation

---
+++ src/main/java/com/roommanagement/services/AvailabilityService.java
@@ -0,0 +1,12 @@
+package com.roommanagement.services;
+
+import java.util.Date;
+import java.util.List;
+import com.roommanagement.beans.Bookings;
+
+public interface AvailabilityService {
+
+	public List<Bookings> getBookingsOfDate(Date date);
+	
+	
+}
