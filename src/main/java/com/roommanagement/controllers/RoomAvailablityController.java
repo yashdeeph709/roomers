df32e96acf28a8344eb061163223f5cd467ae04e
nikam_h <nikam_h@HARSHAL-N.India.XoriantCorp.com>
2015-12-15 20:54:28 +0530

    Completed Availability Test..!!

--- src/main/java/com/roommanagement/controllers/RoomAvailablityController.java
+++ src/main/java/com/roommanagement/controllers/RoomAvailablityController.java
@@ -1,14 +1,11 @@
 package com.roommanagement.controllers;
 
 import java.net.UnknownHostException;
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -16,13 +13,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.roommanagement.beans.Bookings;
-import com.roommanagement.repository.RoomRepository;
 import com.roommanagement.services.AvailabilityService;
 
 @CrossOrigin
@@ -30,24 +25,26 @@ import com.roommanagement.services.AvailabilityService;
 @RequestMapping("/roommanagement")
 public class RoomAvailablityController {
 	
-	
-	
 	@Autowired
 	AvailabilityService availabilityService;
 	
-	@RequestMapping(value = "/availability", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<Bookings>> getBookingOfDate(@RequestHeader String authToken,@RequestBody Map<String,Date> dateStr) throws UnknownHostException {
+	@RequestMapping(value = "/availability/{date}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Bookings>> getBookingOfDate(@RequestHeader String authToken,@PathVariable("date") String strDate) throws UnknownHostException, ParseException {
 		
-		List<Bookings> bookingsList = availabilityService.getBookingsOfDate(dateStr.get("date"));
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+		Date date = format.parse(strDate);
+		List<Bookings> bookingsList = availabilityService.getBookingsOfDate(date);
 		HttpHeaders httpHeaders = new HttpHeaders();
 
 		return new ResponseEntity<List<Bookings>>(bookingsList, httpHeaders,HttpStatus.ACCEPTED);
 	}
 	
-	@RequestMapping(value = "/availabilities", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Map<Integer,List<Bookings>>> bookingsOfRange(@RequestHeader String authToken,@RequestBody Map<String,Date> dateStr) throws ParseException, UnknownHostException{
+	@RequestMapping(value = "/availabilities/{date}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Map<Integer,List<Bookings>>> bookingsOfRange(@RequestHeader String authToken,@PathVariable("date") String strDate) throws ParseException, UnknownHostException{
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+		Date date = format.parse(strDate);
 		HttpHeaders httpHeaders = new HttpHeaders();
-		return new ResponseEntity<Map<Integer,List<Bookings>>>(availabilityService.bookingsOfRange(dateStr.get("date")),httpHeaders,HttpStatus.ACCEPTED);
+		return new ResponseEntity<Map<Integer,List<Bookings>>>(availabilityService.bookingsOfRange(date),httpHeaders,HttpStatus.ACCEPTED);
 	} 
 
 }
---
+++ src/test/java/com/roommanagement/RoomAvailabilityTest.java
@@ -0,0 +1,70 @@
+package com.roommanagement;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+public class RoomAvailabilityTest {
+
+	Client client;
+	String baseURI = "http://localhost:8080/roommanagement";
+	WebResource webResource;
+	ClientResponse response;
+	
+	@Before
+	public void setUp() throws Exception {
+		client=Client.create();
+	}
+
+	@Test
+	public void testAvailabilty() {
+		
+		webResource=client.resource(baseURI+"/availability/2015-10-06T10:27:56");
+		ClientResponse response = webResource.type("application/json").header("authToken","56685db316697f79e253431d").get(ClientResponse.class);
+		assertEquals(202,response.getStatus());
+	}
+	
+	@Test
+	public void testAvailabiltyWithoutDate() {
+		
+		webResource=client.resource(baseURI+"/availability/");
+		ClientResponse response = webResource.type("application/json").header("authToken","56685db316697f79e253431d").get(ClientResponse.class);
+		assertEquals(404,response.getStatus());
+	}
+	
+	@Test
+	public void testAvailabilties() {
+		
+		webResource=client.resource(baseURI+"/availabilities/2015-10-06T10:27:56");
+		ClientResponse response = webResource.type("application/json").header("authToken","56685db316697f79e253431d").get(ClientResponse.class);
+		assertEquals(202,response.getStatus());
+	}
+	
+	@Test
+	public void testAvailabiltiesWithoutDate() {
+		
+		webResource=client.resource(baseURI+"/availabilities/");
+		ClientResponse response = webResource.type("application/json").header("authToken","56685db316697f79e253431d").get(ClientResponse.class);
+		assertEquals(404,response.getStatus());
+	}
+
+	@Test(expected = AssertionError.class)
+	public void testAvailabiltyWithWrongDateFormat() {
+		
+		webResource=client.resource(baseURI+"/availability/546278436278342");
+		ClientResponse response = webResource.type("application/json").header("authToken","56685db316697f79e253431d").get(ClientResponse.class);
+		assertEquals(404,response.getStatus());
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void testAvailabiltiesWithWrongDateFormat() {
+		
+		webResource=client.resource(baseURI+"/availabilities/204593-126");
+		ClientResponse response = webResource.type("application/json").header("authToken","56685db316697f79e253431d").get(ClientResponse.class);
+		assertEquals(404,response.getStatus());
+	}
+	
+}
