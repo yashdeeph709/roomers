0962a19503af18ab115b544f2928a31270f0ae42
kumar_ni <kumar_ni@Nihit-K.India.XoriantCorp.com>
2015-12-15 11:58:59 +0530

    delete Room test done!

--- src/test/java/com/roommanagement/RoomDeleteTest.java
+++ src/test/java/com/roommanagement/RoomDeleteTest.java
@@ -9,7 +9,6 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
-
 public class RoomDeleteTest {
 	Client client;
 	WebResource webResource;
@@ -25,10 +24,10 @@ public class RoomDeleteTest {
 	public void testdeleteRoom() {
 		client = Client.create();
 		webResource = client.resource("http://localhost:8080/roommanagement/room/566a580e63c7c445a5d2863c");
-		ClientResponse response = webResource.accept("application/json").header("authToken","Bahar")
+		ClientResponse response = webResource.accept("application/json").header("authToken", "Bahar")
 				.delete(ClientResponse.class);
 		String expected = "";
 		String actual = response.getEntity(String.class);
-		assertEquals(expected,actual);
+		assertEquals(expected, actual);
 	}
 }
