9a7bb8e843c0528da1edda6a0d4df23a7eeea23b
nikam_h <nikam_h@HARSHAL-N.India.XoriantCorp.com>
2015-12-15 11:36:25 +0530

    User Pagination Added..!!

--- src/main/java/com/roommanagement/controllers/UsersController.java
+++ src/main/java/com/roommanagement/controllers/UsersController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.roommanagement.beans.Room;
 import com.roommanagement.beans.User;
 import com.roommanagement.collections.UserCollection;
 import com.roommanagement.services.UserService;
@@ -37,8 +39,8 @@ public class UsersController {
 			return new ResponseEntity<List<UserCollection>>(users,responseHeaders,httpstatus);
 	}
 
-	@RequestMapping(value="/user/{MongoId}", method=RequestMethod.DELETE,produces=MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody ResponseEntity<String> deletUser(@RequestHeader String authToken,@PathVariable("MongoId") String id) {
+	@RequestMapping(value="/user/{id}", method=RequestMethod.DELETE,produces=MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody ResponseEntity<String> deletUser(@RequestHeader String authToken,@PathVariable("id") String id) {
 		service.delete(id);
 		httpstatus=HttpStatus.ACCEPTED;
 		return new ResponseEntity<String>(responseHeaders,httpstatus);		
@@ -49,5 +51,13 @@ public class UsersController {
 	@RequestMapping(value="/user", method=RequestMethod.POST, produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody ResponseEntity<User> createUser(@RequestHeader String authToken,@RequestBody User user) {
 				return service.getStatus(service.insert(new UserCollection(user)));
+	}
+	
+	@RequestMapping(value="/user/{start}/{end}", method=RequestMethod.GET,produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<User>> roomRange(@RequestHeader String authToken,@PathVariable("start") int start,@PathVariable("end") int end) {
+		
+	
+		return new ResponseEntity<List<User>>(service.userRange(start, end), responseHeaders, HttpStatus.ACCEPTED);
+
 	}
 }
