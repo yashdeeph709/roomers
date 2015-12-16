9396f23484e870e3f030c6314615b4b5ababbf61
urkude_s <urkude_s@SHRUTI-U.India.XoriantCorp.com>
2015-12-16 11:13:46 +0530

    added mail dependencies

--- src/main/java/com/roommanagement/beans/Email.java
+++ src/main/java/com/roommanagement/beans/Email.java
@@ -1,32 +1,10 @@
 package com.roommanagement.beans;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 
 public class Email {
-	
-//	private MailSender mailSender;
-//	private SimpleMailMessage simpleMailMessage;
-//	
-//	public void setSimpleMailMessage(SimpleMailMessage simpleMailMessage) {
-//		this.simpleMailMessage = simpleMailMessage;
-//	}
-//
-//	public void setMailSender(MailSender mailSender) {
-//		this.mailSender = mailSender;
-//	}
-//	
-//	public void sendMail(String dear, String content) {
-//		SimpleMailMessage message = new SimpleMailMessage(simpleMailMessage);
-//		
-//		message.setText(String.format(
-//				simpleMailMessage.getText(), dear, content));
-//
-//		mailSender.send(message);
-//		
-//	}
-//	
+
 	private MailSender mailSender;
 	
 	public void setMailSender(MailSender mailSender) {
